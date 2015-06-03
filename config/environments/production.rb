f0d54c435aa9c70481dc953c6dc2664d36aeb070
Daniela Retelny <dretelny@stanford.edu>
2015-06-02 20:38:07 -0700

    changed to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,27 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #     address: 'smtp.gmail.com',
+  #     port: '587',
+  #     domain: 'gmail.com',
+  #     authentication: 'plain',
+  #     enable_starttls_auto: true,
+  #     user_name: ENV['GMAIL_USERNAME'],
+  #     password: ENV['GMAIL_PASSWORD']
+  # }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: '587',
-      domain: 'gmail.com',
-      authentication: 'plain',
-      enable_starttls_auto: true,
-      user_name: ENV['GMAIL_USERNAME'],
-      password: ENV['GMAIL_PASSWORD']
-  }
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
