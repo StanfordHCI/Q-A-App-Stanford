53bfcb04636606448fb785661f74b1b92d7f74e2
Hassan Ahmed <hassan.a.murtaza@gmail.com>
2015-06-03 21:10:17 +0500

    fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( bootstrap-tagsinput.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap-tagsinput.min.js bootstrap-filestyle.min.js )
